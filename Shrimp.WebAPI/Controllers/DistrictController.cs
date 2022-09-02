1124c1df57c19edef84b98f82674deb9287987f9
Zack Barrell <zpbarrell@gmail.com>
2022-09-01 20:01:24 -0400

    district service injected into district controller

---
+++ Shrimp.WebAPI/Controllers/DistrictController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shrimp.Services.District;
+
+namespace Shrimp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistrictController : ControllerBase
+    {
+        private readonly IDistrictService _districtService;
+        public DistrictController(IDistrictService districtService)
+        {
+            _districtService = districtService;
+        }
+    }
+}
