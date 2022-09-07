a32208409641000ad93753212dd2c57965013b0a
Natassia Caldwell <natassia.caldwell@gmail.com>
2022-09-06 20:57:16 -0400

    school controller create

--- Shrimp.WebAPI/Controllers/SchoolController.cs
+++ Shrimp.WebAPI/Controllers/SchoolController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Shrimp.Models.School;
 using Shrimp.Services.School;
 
 namespace Shrimp.WebAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class SchoolController : Controller
     {
-        private readonly ISchoolService _service;
-        public SchoolController(ISchoolService service)
+        private readonly ISchoolService _schoolService;
+        public SchoolController(ISchoolService schoolService)
         {
-            _service = service;
+            _schoolService = schoolService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSchool([FromBody] SchoolCreate school)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if(await _schoolService.CreateSchoolAsync(school))
+                return Ok("School was successfully added.");
+            
+            return BadRequest("School could not be added at this time");
         }
     }
 }
