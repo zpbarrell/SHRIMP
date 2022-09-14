d8ebf2ce8833bd01173338e3f218680fdf884430
Zack Barrell <zpbarrell@gmail.com>
2022-09-13 20:56:00 -0400

    ReadMe.md file added, testing with enum casting

--- Shrimp.Data/Enums/DistrictEnums.cs
+++ Shrimp.Data/Enums/DistrictEnums.cs
@@ -1,6 +1,27 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Shrimp.Data.Enums
 {
-    public enum DistrictName { Skynet, UmbrellaCorp, BlackMesa, TheInstitute }
+    public enum DistrictName 
+    { 
+        // [Display(Name = "Sky net")] Skynet,
+        // [Display(Name = "Umbrella Corp")] UmbrellaCorp,
+        // [Display(Name = "Black Mesa")] BlackMesa,
+        // [Display(Name = "The Institute")] TheInstitute,
+        [Description("Skynet")]
+        Skynet,
+        [Description("Umbrella Corp")]
+        UmbrellaCorp,
+        [Description("Black Mesa")]
+        Black,
+        [Description("The Institute")]
+        TheInstitute
+        // Skynet = 0,
+        // UmbrellaCorp = 1,
+        // BlackMesa = 2,
+        // TheInstitute = 3
+    }
     public enum DressCode { Strict, Moderate, Comfortable }
     public enum Resources { Water, Agriculture, Lumber, Machinery }
     public enum Permits { Weapons, Hunting, Communications, Labor }
--- Shrimp.Models/District/DistrictCreate.cs
+++ Shrimp.Models/District/DistrictCreate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Shrimp.Data.Enums;
@@ -8,6 +9,7 @@ namespace Shrimp.Models.District
 {
     public class DistrictCreate
     {
+        [Display(Name = "NameOfDistrict")]
         public DistrictName NameOfDistrict { get; set; }
         public decimal CrimeRate { get; set; }
         public int Curfew { get; set; }
